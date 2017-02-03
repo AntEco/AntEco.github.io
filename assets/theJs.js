37f3c11e82ab07228a0b7b0d997fa3df31794b2a
Anton Economos <anton.economos@gmail.com>
2017-02-03 02:02:24 -0500

    added loading screen

--- assets/theJs.js
+++ assets/theJs.js
@@ -1,3 +1,23 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
+
 $(document).ready(function() {
 
 //smooth scrolls to link without changing the url
